9e2b28fe57addac13e7d699f9e102c151ee2c9fa
Thiện Trí Bùi <73270915+tri898@users.noreply.github.com>
2021-06-21 17:00:39 +0700

    fix lỗi nhỏ xíu

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\AuthAdminController;
-use App\Http\Controllers\ AuthUserController;
-use App\Http\Controllers\ AdminController;
-use App\Http\Controllers\ UserController;
+use App\Http\Controllers\AuthUserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
